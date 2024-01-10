756809d10697fdda6d628def0f012385b3c7c809
Sumeddha <2100031034@kluniversity.in>
2024-01-10 11:52:59 +0530

    commit

---
+++ add.py
@@ -0,0 +1,11 @@
+#This is the program of addition of 2 nos in python
+
+n1 = 1.5
+n2 = 6.3
+
+#Add of 2 nos
+sum = n1+n2
+
+print('The Sum of {0} and {1} is {2}'.format(n1,n2,sum)
+[D[D[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C)
+
